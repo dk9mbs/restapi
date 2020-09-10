9398ebb724b4531b4bc9800eacb366bfcc1f49cc
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2020-09-10 20:50:15 +0200

    init.sh changeged for use as source

--- init.sh
+++ init.sh
@@ -1,16 +1,28 @@
 #/bin/bash
 
-BASEDIR=$(realpath $0)
+VENV=~/venv/lab/bin/activate
+PLUGINPATH=~/.restapi/plugins
+
+SCRIPTNAME=$BASH_SOURCE
+BASEDIR=$(realpath $SCRIPTNAME)
 BASEDIR=$(dirname $BASEDIR)
-echo "====================="
-echo "$BASEDIR"
-echo "====================="
 
-mkdir -p ~/.restapi/plugins
-#mkdir -p /usr/local/restapi_plugins
+echo "=============================================================="
+echo "\$0...............:$0"
+echo "Scriptname.......:$SCRIPTNAME"
+echo "Basedir..........:$BASEDIR"
+echo "=============================================================="
 
+mkdir -p "$PLUGINPATH"
 
-. ~/venv/lab/bin/activate
-export PYTHONPATH=/usr/local/restapi_plugins:~/.restapi/plugins:../:/tmp
+if [ -f "$VENV" ];
+then
+    echo "Activate venv environment"
+    . $VENV
+else
+    echo "venv environment not found ($VENV)"
+fi
+
+export PYTHONPATH=/usr/local/restapi_plugins:~/.restapi/plugins:../:/tmp:$BASEDIR
 
 
---
+++ test/test_database_crud.py
@@ -0,0 +1,100 @@
+import unittest
+
+from core.database import CommandBuilderFactory
+from core.database import FetchXmlParser
+from config import CONFIG
+from core.appinfo import AppInfo
+from core.fetchxmlparser import FetchXmlParser
+from services.database import DatabaseServices
+
+class TestFetchxmlParser(unittest.TestCase):
+    def setUp(self):
+        AppInfo.init(__name__, CONFIG['default'])
+        session_id=AppInfo.login("root","password")
+        self.context=AppInfo.create_context(session_id)
+
+    def test_exec(self):
+        xml=f"""
+        <restapi type="delete">
+            <table name="dummy"/>
+            <filter>
+                <condition field="id" value="99"/>
+            </filter>
+        </restapi>
+        """
+        fetch=FetchXmlParser(xml)
+        DatabaseServices.exec(fetch,self.context)
+
+        xml=f"""
+        <restapi type="select">
+            <table name="dummy"/>
+            <filter>
+                <condition field="id" value="99"/>
+            </filter>
+        </restapi>
+        """
+        fetch=FetchXmlParser(xml)
+        dummy=DatabaseServices.exec(fetch,self.context, fetch_mode=1)
+        self.assertIsNone(dummy.get_result())
+
+        xml=f"""
+        <restapi type="insert">
+            <table name="dummy"/>
+            <filter>
+                <condition field="id" value="99"/>
+            </filter>
+            <fields>
+                <field name="id" value="99"/>
+                <field name="name" value="IC735"/>
+                <field name="port" value="3306"/>
+            </fields>
+        </restapi>
+        """
+        fetch=FetchXmlParser(xml)
+        DatabaseServices.exec(fetch,self.context)
+
+        xml=f"""
+        <restapi type="select">
+            <table name="dummy"/>
+            <filter>
+                <condition field="id" value="99"/>
+            </filter>
+        </restapi>
+        """
+        fetch=FetchXmlParser(xml)
+        dummy=DatabaseServices.exec(fetch,self.context, fetch_mode=1)
+        self.assertIsNotNone (dummy.get_result())
+
+        xml=f"""
+        <restapi type="update">
+            <table name="dummy"/>
+            <filter>
+                <condition field="id" value="99"/>
+            </filter>
+            <fields>
+                <field name="name" value="UPDATE"/>
+            </fields>
+        </restapi>
+        """
+        fetch=FetchXmlParser(xml)
+        DatabaseServices.exec(fetch,self.context)
+
+        xml=f"""
+        <restapi type="select">
+            <table name="dummy"/>
+            <filter>
+                <condition field="id" value="99"/>
+            </filter>
+        </restapi>
+        """
+        fetch=FetchXmlParser(xml)
+        dummy=DatabaseServices.exec(fetch,self.context, fetch_mode=1)
+        self.assertEqual(dummy.get_result()['name'], "UPDATE")
+
+    def tearDown(self):
+        AppInfo.save_context(self.context, True)
+        AppInfo.logoff(self.context)
+
+
+if __name__ == '__main__':
+    unittest.main()
