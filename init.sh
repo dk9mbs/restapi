8e93e38b3e6b0a344be545e8ba3a1f540912e904
dk9mbs <dk9mbs@heiligenhafen.the-b.eu>
2020-09-14 11:01:01 +0200

    init.sh script changed

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+
+
 VENV=~/venv/restapi/bin/activate
 VENVALT=~/venv/lab/bin/activate
 PLUGINPATH=~/.restapi/plugins
@@ -12,7 +14,10 @@ echo "=============================================================="
 echo "\$0...............:$0"
 echo "Scriptname.......:$SCRIPTNAME"
 echo "Basedir..........:$BASEDIR"
-echo "RESTAPIPATH......:$BASEDIR"
+export PYTHONPATH=$BASEDIR:~/.restapi/plugins:../:/tmp
+export RESTAPIPATH=$BASEDIR
+
+echo "PYTHONPATH.......:$PYTHONPATH"
 echo "=============================================================="
 
 mkdir -p "$PLUGINPATH"
@@ -23,16 +28,13 @@ then
     . $VENV
 else
     echo "default venv environment not found ($VENV)"
+    if [ -f "$VENVALT" ];
+    then
+        echo "activate alternative venv environment ($VENVALT)"
+        . $VENVALT
+    else
+        echo "alternative venv environment not found ($VENVALT)"
+    fi
 fi
 
-if [ -f "$VENVALT" ];
-then
-    echo "activate alternative venv environment ($VENVALT)"
-    . $VENVALT
-else
-    echo "alternative venv environment not found ($VENVALT)"
-fi
-
-export PYTHONPATH=$BASEDIR:/usr/local/restapi_plugins:~/.restapi/plugins:../:/tmp:$BASEDIR/examples
-export RESTAPIPATH=$BASEDIR
 
