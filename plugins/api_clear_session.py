8dade9a7ea1e368b899456f779df4115ae3955b8
dk9mbs <dk9mbs@gmail.com>
2022-12-17 22:11:38 +0100

    clear all sessions older then 48h

--- plugins/api_clear_session.py
+++ plugins/api_clear_session.py
@@ -12,7 +12,6 @@ def execute(context, plugin_context, params):
     <restapi type="delete">
         <table name="api_session"/>
         <filter type="AND">
-            <condition field="disabled" value="-1"/>
             <condition field="last_access_on" operator="olderThenXHours" value="48"/>
         </filter>
     </restapi>
