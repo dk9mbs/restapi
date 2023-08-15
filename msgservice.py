94546e49f1fd82c781d9926dd64ed4cb3838a9f9
dk9mbs <dk9mbs@gmail.com>
2023-08-15 21:42:22 +0200

    ping implemented

--- msgservice.py
+++ msgservice.py
@@ -48,6 +48,7 @@ class MqttWorker():
         self._topics.append({"topic": "owntracks/+/+","regex":"^owntracks/.*/.*$", "prefix":""})
         self._topics.append({"topic": "owntracks/+/+/waypoints","regex":"^owntracks/.*/.*/waypoints$", "prefix":""})
         self._topics.append({"topic": "+/rpc","regex":"^shelly.*/rpc$", "prefix":"iot_shelly/"})
+        self._topics.append({"topic": "restapi/sys/ping","regex":"^restapi/sys/ping$", "prefix":""})
 
     def kill(self):
         self._run=False
